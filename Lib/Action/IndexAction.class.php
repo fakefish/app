5bb0b32c1e53c3d6400820ebe90aa863f7fd35a3
Fakefish <fakefish.wjy@gmail.com>
2012-11-10 15:09:43 +0800

    nothing

--- Lib/Action/IndexAction.class.php
+++ Lib/Action/IndexAction.class.php
@@ -7,13 +7,6 @@ class IndexAction extends Action {
         $count = $Form->count();    //计算总数
         $p = new Page($count, 5);
         $list = $Form->limit($p->firstRow . ',' . $p->listRows)->order('id desc')->select();
-        //$p->firstRow 当前页开始记录的下标，$p->listRows 每页显示记录数
-        //一般定义分页样式 通过分页对象的setConfig定义其config属性；
-        /*
-          默认值为$config = array('header'=>'条记录','prev'=>'上一页','next'=>'下一页','first'=>'第一页','last'=>'最后一页',
-          'theme'=>' %totalRow% %header% %nowPage%/%totalPage% 页 %upPage% %downPage% %first%  %prePage%  %linkPage%  %nextPage% %end%');
-          修改显示的元素的话设置theme就行了，可对其元素加class
-         */
         $p->setConfig('header', '条数据');
         $p->setConfig('prev', "前");
         $p->setConfig('next', '后');
